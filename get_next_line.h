d37723308de951775811159032459721cfc22d57
jsegovia <jorge.segovia.gil@gmail.com>
2022-02-23 13:54:51 +0100

    Added forgotten semicolons

--- get_next_line.h
+++ get_next_line.h
@@ -23,9 +23,9 @@
 
 char	*get_next_line(int fd);
 
-char *ft_strchr(const char *s, int c)
-char *ft_strjoin(char const *s1, char const *s2)
-size_t ft_strlen(const char *s)
-char *ft_substr(char const *s, unsigned int start, size_t len)
+char *ft_strchr(const char *s, int c);
+char *ft_strjoin(char const *s1, char const *s2);
+size_t ft_strlen(const char *s);
+char *ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
